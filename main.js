dbc1298d8b8ff88030dd073f25c4c77e2bc06217
Jordan Gregory <jordangregory@gmail.com>
2017-06-15 19:21:23 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+/*
+  Here is a guide for the steps you could take:
+*/
+
+// 1. First select and store the elements you'll be working with
+var container = document.querySelector("container");
+var player = document.quertySelectoe("player");
+var musicPLayer = document.quertySelectoe("music-player");
+var search = document.quertySelectoe("search");
+var searchForm = document.quertySelectoe("search-form");
+var results = document.quertySelectoe("results");
+
+// 2. Create your `onSubmit` event for getting the user's search term
+
+// 3. Create your `fetch` request that is called after a submission
+
+// 4. Create a way to append the fetch results to your page
+
+// 5. Create a way to listen for a click that will play the song in the audio play
